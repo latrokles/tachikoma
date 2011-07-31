b91343ab2070ac1e5232234d6aaa118c6c99f916
latrokles <latrokles@gmail.com>
2011-07-31 00:56:20 -0400

    Rewrote bump_navigation, started writing some code to log tachikoma activities.
    - Rewrote the bump_navigation code to be a bit more straightforward. Still has
      quite a bit of things that may be odd, but they'll be corrected later on.
    
    - Also started on some code to log the tachikoma actions, using video and logging.
      It's not finished yet, but this is going on the next burn.
    
    --l.

--- bump_navigation.py
+++ bump_navigation.py
@@ -1,56 +1,116 @@
 #!/usr/bin/python
 #
 #
+import sys
+# the machine connected to the create has ROS installed so I just
+# import the opencv installed by ROS (why am I not just using ROS ?)
+try:
+    from ros_path import python_path
+    sys.path += python_path
+except ImportError:
+    print 'ROS is not installed on this machine...'
+
 import create
 import time
 import random
+import cv
+from datetime import datetime
+
+class VideoRecorder(object):
+
+    def __init__(self):
+        file = datetime.now().isoformat().replace(':','_').replace('T','__')
+        self.camera   = cv.CaptureFromCAM(0)
+        self.recorder = cv.CreateVideoWriter(
+                                             file + '.mp4',
+                                             cv.CV_FOURCC('M', 'P', '4', '2'),
+                                             30,
+                                             (320, 240),
+                                             1
+                                            )
+    def record(self):
+        while cv.WaitKey(33) != 27:
+            frame = cv.QueryFrame(self.camera)
+            cv.WriteFrame(self.recorder, frame)
 
 PORT = '/dev/ttyUSB0'
 class Tachikoma(object):
     '''
     '''
     def __init__(self, port):
-        self.robot = create.Create(port)
-        self.speed = 0
-
-    def set_speed(self, speed):
         '''
-        Set the speed and move
+        Instantiate Create object at port, set initial state variables, and
+        start vision system.
         '''
-        self.speed = speed
-        self.robot.go(speed, 0)
+        self.bot    = create.Create(port)
+        self.stopped = True
 
     def forward(self, speed):
-        if speed < 0: speed = -speed
-        self.set_speed(speed)
+        '''
+        Move forward at speed.
+        '''
+        self.bot.go(speed)
+        self.stopped = False
 
     def backward(self, speed):
-        if speed > 0: speed = -speed
-        self.set_speed(speed)
+        '''
+        Move backward at speed.
+        '''
+        self.bot.go(-speed)
+        self.stopped = False
 
-    def turn(self, angle):
-        self.robot.turn(angle, angle*2)
+    def turn(self, direction, degrees):
+        '''
+        Turn in 'degrees' in 'direction'. Directions can be LEFT, RIGHT, or
+        BACK (turn 180 degrees)
+        '''
+        # moving to the right needs negative value for degrees
+        if direction == 'RIGHT': degrees = -degrees
+        if direction == 'BACK': degrees = 180
+        speed = degrees * 2
+        self.bot.turn(degrees, speed)
+
+    def check_and_avoid_collision(self):
+        '''
+        If left bumper is activated back away a bit and turn to the right, if
+        right bumper is activated, back awaay and turn to the left.
+        '''
+        sensors = self.bot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP])
+        if sensors[create.LEFT_BUMP] == 1 and sensors[create.RIGHT_BUMP] == 0:
+            self.avoid_collision('RIGHT')
+        elif sensors[create.RIGHT_BUMP] == 1 and sensors[create.LEFT_BUMP] == 0:
+            self.avoid_collision('LEFT')
+        elif sensors[create.RIGHT_BUMP] ==1 and sensors[create.LEFT_BUMP] == 1:
+            self.avoid_collision('BACK')
+
+    def avoid_collision(self, direction):
+        '''
+        Back away and turn in direction.
+        '''
+        self.backward(30)
+        time.sleep(0.5)
+        self.turn(direction, 30)
 
     def stop(self):
-        self.robot.stop()
-
-    def check_obstacle(self):
-        sensor_values = self.robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP])
-        if sensor_values[create.LEFT_BUMP] == 1 or sensor_values[create.RIGHT_BUMP] == 1:
-            self.stop()
-            self.backward(30)
-            time.sleep(0.5)
-            self.turn(random.choice([-30, 30, 180]))
-            self.forward(30)
-
-    def navigate(self):
-        self.set_speed(40)
-        while True:
-            self.check_obstacle()
+        '''
+        Stop robot.
+        '''
+        self.bot.stop()
+        self.stopped = True
 
     def shutdown(self):
-        self.set_speed(0)
-        self.robot.close()
+        '''
+        Stops the robot (if it hasn't stopped before) and closes down the
+        connection.
+        '''
+        if not self.stopped:
+            self.stop()
+        self.bot.close()
+
+    def navigate(self):
+        while True:
+            self.forward(50)
+            self.check_and_avoid_collision()
 
 if __name__ == '__main__':
     max_bot = Tachikoma(PORT)
