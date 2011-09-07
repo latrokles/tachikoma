4d983a3da16fc0a81ed42dc86ce52eeddd1e7677
latrokles <latrokles@gmail.com>
2011-09-07 01:52:00 -0400

    Most of TeleoperationClient done, started server
    - A large part of the interface and the flow for the TeleoperationClient
      is done. There are a few things to work out (see below), but I figured
      I'd start work on TeleoperationServer to be able to test things
      quickly and continue building accordingly.
    
    - The way the commands will work is that as a key is pressed in the
      client's UI a command to start moving forward (FWD) will be sent. This
      command will call the forward command on the tachikoma, which will set
      it in motion until a command to stop moving (STPM) is received. This
      stop command will be sent by the TeleoperationClient the moment the
      user releases the up key. This behavior will be the same for all other
      control options.
    
    - The only command that doesn't behave in a 'sticky' manner is the
      command to play the horn (HNK).
    
    - The interface in TeleoperationClient also provides some feedback to
      the user by displaying an arrow indicating the direction the robot is
      moving. In the future it should display battery information and the
      general location of obstacles the robot bumps into... I'll get there.
    
    - At the moment, all the TeleoperationServer is doing is receiving the
      commands from the TeleoperationClient and displaying them on the
      screen. I have began thinking about a couple of modifications I need
      to make on the tachikoma module in order to get some of the behavior
      mentioned above... it's ok, it was needed for some of the autonomous
      things I  want to do anyway.
    
    - It's pretty clear from looking at the code and reading this commit log
      that there is no security whatsoever as far as this whole
      teleoperation module goes (needs a name change too, btw). I'm just
      tinkering with this on a local network and in case someone does try to
      subvert the robot... let's just say they'd be well within reach...
    
    - On a more serious note about that security thing, there are a couple
      of things I've been thinking about, and I'm thinking of implementing
      them in time, but at the moment I'm more concerned with testing this
      implementation of teleoperation.
    
    - I also need to redo the graphics for the UI feedback mechanism, saved
      them as JPEGS, which messed things up when I tried to set the colorkey
      with pygame.
    
    - On the note of the UI, I may have to keep track of what buttons are
      pressed in order to display the UI notifications for them
      individually. There's a bug right now in which if a user is pressing
      the up and left arrows, the forward and turn left notifications show
      up on the screen, however if the user releases a single key, they both
      disappear from the screen (the command for the key that is still
      pressed remains in effect though). I can easily fix this by marking
      the notifications that should be displayed or not individually and
      just do a blanket display inside for the "active" commands in a
      function of its own (display_widgets, for example).
    
    That's all for now, going to sleep.
    --l.

--- experimental/remote_control.py
+++ experimental/remote_control.py
@@ -3,21 +3,68 @@
 '''
 Remote operation of the irobot create robot in the form of a server that receives
 command instructions and a client (with a graphical interface) that issues them.
+
+The commands are rather simple at the moment:
+    - FWD:  start moving forward
+    - BCK:  start moving backward
+    - RGT:  start turning right
+    - LFT:  start turning left
+    - STPM: stop moving (forward/backward)
+    - STPT: stop turning (left/right)
+    - HNK:  sound the horn
+
+As for the data sent by the robot:
+    - OLFT: indicates there's an obstacle to the left (0 or 1)
+    - ORGT: indicates there's an obstacle on the right (0 or 1)
+    - OFRT: indicates there's an obstacle completely in front (0 or 1)
+    - BATT: battery (0-100)
 '''
 import sys
 import json
 import socket
+import tachikoma
 import pygame
 from pygame.locals import *
 
 ## -- SOME GLOBALS -- ##
 MOVING_SPEED = 20 # cm/s
 TURN_SPEED   = 10 # deg/s
-SIZE  = WIDTH, HEIGHT = 480, 320
+SIZE  = WIDTH, HEIGHT = 480, 360
 UI_BG = '../data/img/background.png'
+FWD_ARROW = '../data/img/forward_arrow.jpg'
+BCK_ARROW = '../data/img/backward_arrow.jpg'
+LFT_ARROW = '../data/img/left_arrow.jpg'
+RGT_ARROW = '../data/img/right_arrow.jpg'
+
+def load_image(img_file):
+    '''
+    Returns a pygame surface for img_file
+    '''
+    img = pygame.image.load(img_file).convert()
+    img.set_colorkey((0, 0, 0), RLEACCEL)
+    return img
 
 class TeleoperationServer(object):
-    pass
+    '''
+    Receives instructions from the TeleoperationClient and controls the robot
+    accordingly. It also queries the robot for data about its sensors and
+    battery life (just another sensor) and sends it to the TeleoperationClient
+    so that the operator is aware of the state of the robot.
+    '''
+    def __init__(self, port=8075):
+        self.addr  = '0.0.0.0'
+        self.port  = port
+        #self.robot = tachikoma.Tachikoma(tachikoma.PORT)
+        self.socket_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_in.bind((self.addr, self.port))
+
+    def receive_command(self):
+        command, addr = self.socket_in.recvfrom(1024)
+        print(command + "from ", addr)
+
+    def run(self):
+        while True:
+            self.receive_command()
 
 
 class TeleoperationClient(object):
@@ -29,10 +76,19 @@ class TeleoperationClient(object):
     def __init__(self, server_addr, server_port=8075):
         self.server_addr = server_addr
         self.server_port = server_port
-        # create our window
+        self.socket_out  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # create our window and UI stuff
         self.window      = pygame.display.set_mode(SIZE)
         self.background  = pygame.image.load(UI_BG)
+        self.ui_elements = {
+                           'FWD':(load_image(FWD_ARROW), ((WIDTH/2) - 50, 20)),
+                           'BCK':(load_image(BCK_ARROW), ((WIDTH/2) - 50, 120)),
+                           'LFT':(load_image(LFT_ARROW), (WIDTH - 120, 20)),
+                           'RGT':(load_image(RGT_ARROW), (20, 20)),
+                           'HNK':(load_image(RGT_ARROW), ((WIDTH/2) - 50, 180)),
+                           }
         pygame.display.set_caption('Teleoperation Client: ' + self.server_addr)
+        self.clear_ui()
 
     def process_event(self, event):
         '''
@@ -42,32 +98,49 @@ class TeleoperationClient(object):
         #print(event) # a bit of debugging data
         if event.type == pygame.QUIT: sys.exit(0)  # handle termination first
         if event.type == KEYDOWN:
-            if event.key == K_UP:    print('Move forward')
-            if event.key == K_DOWN:  print('Move backward')
-            if event.key == K_LEFT:  print('Turning left')
-            if event.key == K_RIGHT: print('Turning right')
-            if event.key == K_SPACE: print('honk')
-        if event.type == KEYUP:
-            if event.key == K_UP:    print('stop forward')
-            if event.key == K_DOWN:  print('stop backward')
-            if event.key == K_LEFT:  print('stop urning left')
-            if event.key == K_RIGHT: print('stop Turning right')
+            if event.key == K_UP:    self.send_command('FWD')
+            if event.key == K_DOWN:  self.send_command('BCK')
+            if event.key == K_LEFT:  self.send_command('LFT')
+            if event.key == K_RIGHT: self.send_command('RGT')
+            if event.key == K_SPACE: self.send_command('HNK')
 
-    def display_ui_elements(self):
+        if event.type == KEYUP:
+            if event.key == K_UP or event.key == K_DOWN:
+                self.send_command('STPM')
+            if event.key == K_LEFT or event.key == K_RIGHT:
+                self.send_command('STPT')
+            if event.key == K_SPACE:
+                self.send_command('STPH')
+
+    def send_command(self, command):
         '''
-        Displays the different UI elements on the screen.
+        Sends the command over the network and updates UI
         '''
-        pass
+        self.socket_out.sendto(command, (self.server_addr, self.server_port))
+        if command[:3] == 'STP':
+            self.clear_ui()
+        else:
+            ui_element = self.ui_elements[command]
+            self.window.blit(ui_element[0], ui_element[1])
+
+    def clear_ui(self):
+        '''
+        Clears the UI indicators
+        '''
+        self.window.blit(self.background, (0, 20))
 
     def run(self):
         while True:
             # get events and process them accordingly
             for event in pygame.event.get():
                 self.process_event(event)
-            self.window.blit(self.background, (0, 0))
-            #pygame.draw.rect(self.window, (255, 255, 255), (20, 20, 100, 100))
             pygame.display.flip()
 
 if __name__ == '__main__':
-    client = TeleoperationClient('0.0.0.0')
-    client.run()
+    if sys.argv[1] == 'client':
+        addr = sys.argv[2]
+        client = TeleoperationClient(addr)
+        client.run()
+    elif sys.argv[1] == 'server':
+        server = TeleoperationServer()
+        server.run()
